83b0170a7318498e7b7c859c27643b96dc5aec49
Benyamin Radmard <b.radmard77@gmai.com>
2023-05-01 19:49:17 +0200

    mehr styling für die views

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ let sessionID;
 navigateTo();
 
 function navigateTo(pageName='register') {
-    var includes = $('[data-include]');
-    $(this).addClass('active');
+    let includes = $('[data-include]');
     $.each(includes, function () {
         if($(this).data('include') !== 'navbar'){
             $(this).addClass('d-none');
         }
-        if($(this).data('include') === pageName)
+        if($(this).data('include') === pageName){
             $(this).removeClass('d-none');
+        }
     });
+    $(this).addClass('active');
 }
 
 
